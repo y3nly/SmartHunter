fbaca37619769d2a4bc0145b69ac8df25e145e5f
azure <rz7@outlook.jp>
2019-10-30 20:28:09 +0900

    Support Minimize to Task Tray

--- SmartHunter/Ui/Windows/ConsoleWindow.xaml.cs
+++ SmartHunter/Ui/Windows/ConsoleWindow.xaml.cs
@@ -5,11 +5,66 @@ namespace SmartHunter.Ui.Windows
 {
     public partial class ConsoleWindow : Window
     {
+
+        System.Windows.Forms.NotifyIcon TrayIcon { get; }
+
+        void MinimizeToTray()
+        {
+            IsEnabled = false;
+            Hide();
+            ShowInTaskbar = false;
+        }
+
+        void Restore()
+        {
+            IsEnabled = true;
+            Show();
+            WindowState = WindowState.Normal;
+            ShowInTaskbar = true;
+        }
+
         public ConsoleWindow()
         {
             InitializeComponent();
 
             DataContext = ConsoleViewModel.Instance;
+
+            TrayIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath),
+                Visible = true
+            };
+
+            Closed += (s, e) => TrayIcon?.Dispose();
+
+            var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+            contextMenu.Items.Add("E&xit");
+
+            if (contextMenu == null)
+            {
+                System.Diagnostics.Debug.Fail("contextMenu is null");
+            }
+
+            TrayIcon.DoubleClick += (sender, e) =>
+            {
+                if (IsEnabled)
+                {
+                    MinimizeToTray();
+                }
+                else
+                {
+                    Restore();
+                }
+            };
+        }
+
+        protected override void OnStateChanged(System.EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                MinimizeToTray();
+            }
+            base.OnStateChanged(e);
         }
     }
 }
